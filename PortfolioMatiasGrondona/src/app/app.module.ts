be8b66383b2aa369a26e8cad844962e7f175bd95
Matias <44991886+MatiasGrondona@users.noreply.github.com>
2022-08-25 01:40:01 -0300

    Footer
    Se agrego el componente footer y algunas correcciones de estilo.

--- PortfolioMatiasGrondona/src/app/app.module.ts
+++ PortfolioMatiasGrondona/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftSkillComponent } from './components/hard-soft-skill/hard-soft-skill.component';
 import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProjectComponent } from './components/project/project.component';
     ExperienceComponent,
     EducationComponent,
     HardSoftSkillComponent,
-    ProjectComponent
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
