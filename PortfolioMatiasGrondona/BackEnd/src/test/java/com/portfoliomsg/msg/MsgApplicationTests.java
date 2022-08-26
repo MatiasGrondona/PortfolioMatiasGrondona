a3ec2ffb36caa78a6a0a4939084f2c2388367bce
Matias <44991886+MatiasGrondona@users.noreply.github.com>
2022-08-26 00:40:09 -0300

    Reestructuración del proyecto
    Se cambio la estructura del proyecto, se crearon dos carpetas una FrontEnd y otra BackEnd, en la primero se movió todo lo que ya se tenia y en la segunda se agrego el contenido descargado de Spring Initializr.

---
+++ PortfolioMatiasGrondona/BackEnd/src/test/java/com/portfoliomsg/msg/MsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliomsg.msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
