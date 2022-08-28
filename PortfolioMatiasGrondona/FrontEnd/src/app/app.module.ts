af6f6d798df2b338492962393397d9cde71948bf
Matias <44991886+MatiasGrondona@users.noreply.github.com>
2022-08-27 22:05:01 -0300

    Conexión parcial don DB
    Se Realizo una parte de la conexión entre el back y el front.

--- PortfolioMatiasGrondona/FrontEnd/src/app/app.module.ts
+++ PortfolioMatiasGrondona/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftSkillComponent } from './components/hard-soft-skill/hard-soft-skill.component';
 import { ProjectComponent } from './components/project/project.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
