1463cec72a77f54f379d53eb65296f21694b8ea1
Matias <44991886+MatiasGrondona@users.noreply.github.com>
2022-08-30 04:39:15 -0300

    JWT
    Avance un poco mas con la implementación del Java Web Token.

--- PortfolioMatiasGrondona/BackEnd/src/main/java/com/portfoliomsg/msg/Security/Controller/AuthController.java
+++ PortfolioMatiasGrondona/BackEnd/src/main/java/com/portfoliomsg/msg/Security/Controller/AuthController.java
@@ -4,7 +4,11 @@
  */
 package com.portfoliomsg.msg.Security.Controller;
 
+import com.portfoliomsg.msg.Security.Dto.JwtDto;
+import com.portfoliomsg.msg.Security.Dto.LoginUsuario;
+import com.portfoliomsg.msg.Security.Dto.NuevoUsuario;
 import com.portfoliomsg.msg.Security.Entity.Rol;
+import com.portfoliomsg.msg.Security.Entity.Usuario;
 import com.portfoliomsg.msg.Security.Enums.RolNombre;
 import com.portfoliomsg.msg.Security.Service.RolService;
 import com.portfoliomsg.msg.Security.Service.UsuarioService;
@@ -18,6 +22,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -55,32 +61,41 @@ public class AuthController {
             return new ResponseEntity(new Mensaje("Campo mal puesto o Email invalido!"), HttpStatus.BAD_REQUEST);
         }
         
-        if(usuarioService.existByNombreUsuario(nombreUsuario.getNombreUsuario())){
+        if(usuarioService.existByNombreUsuario(nuevoUsuario.getNombreUsuario())){
             return new ResponseEntity(new Mensaje("Ese nombre de usuario ya existe!"), HttpStatus.BAD_REQUEST);
         }
         
-        if(usuarioService.existByEmail(nombreUsuario.getEmail())){
+        if(usuarioService.existByEmail(nuevoUsuario.getEmail())){
             return new ResponseEntity(new Mensaje("Ese Email ya esta en uso!"), HttpStatus.BAD_REQUEST);
         }
         
         Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
         
-        Set<Rol> roles = HashSet<>();
+        Set<Rol> roles = new HashSet<>();
         roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
         
         if(nuevoUsuario.getRoles().contains("admin")){
             roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
             usuario.setRoles(roles);
             usuarioService.save(usuario);
-            
-            return new ResponseEntity(new Mensaje("Usuario guardado!"), HttpStatus.CREATED);
         }
+        return new ResponseEntity(new Mensaje("Usuario guardado!"), HttpStatus.CREATED);
     }
     
-    public ResponseEntity<JdtDTO> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-            return new ResponseEntity(new Mensaje("Campos mal Puestos!"), HttpStatus.BAD_REQUEST);
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken);
+            return new ResponseEntity(new Mensaje("Campos mal Puestos!"), HttpStatus.BAD_REQUEST);  
         }
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetail = (UserDetails) authentication.getPrincipal();
+        JwtDto jwtDto = new JwtDto(jwt, userDetail.getUsername(), userDetail.getAuthorities());
+        
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
     }
 }
